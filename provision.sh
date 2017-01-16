3edc45ef2e824b27c3c44b07e9c5fbc06a8f0593
Will Usher <william.usher@ouce.ox.ac.uk>
2017-01-16 16:41:07 +0000

    Added provision steps for solid_waste model

--- provision.sh
+++ provision.sh
@@ -28,3 +28,56 @@ EOF
 
 # Provision script for the transport model
 bash /vagrant/provision/transport.sh
+
+# Provision script for the solid waste model
+
+# Install OS packages
+apt-get install -y tmux
+
+# Create vagrant role if not exists
+su postgres -c "psql -c \"SELECT 1 FROM pg_user WHERE usename = 'runcdamrole';\" " \
+    | grep -q 1 || su postgres -c "psql -c \"CREATE ROLE runcdamrole;\" "
+
+su postgres -c "psql -c \"SELECT 1 FROM pg_user WHERE usename = 'sosadministratorrole';\" " \
+    | grep -q 1 || su postgres -c "psql -c \"CREATE ROLE sosadministratorrole;\" "
+
+su postgres -c "psql -c \"SELECT 1 FROM pg_user WHERE usename = 'djrole';\" " \
+    | grep -q 1 || su postgres -c "psql -c \"CREATE ROLE djrole;\" "
+
+su postgres -c "psql -c \"SELECT 1 FROM pg_user WHERE usename = 'scr1';\" " \
+    | grep -q 1 || su postgres -c "psql -c \"CREATE ROLE scr1;\" "
+
+# # Load db functions and tables
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/cdam_modified_func.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/cdam_admin_update_model_execution_time.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/itrc_sos_test_sw_run_order.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/cdam_admin_auto_run_reports.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/itrc_sos_test_sw_sectors_and_strategies.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/itrc_sos_test_sw_logs_schema.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/itrc_sos_test_sw_emission_factors_and_fuels.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/itrc_sos_test_sw_emission_factors_and_sources.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/itrc_sos_test_sw_lu.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/itrc_sos_test_sw_base_audit.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/itrc_sos_test_sw_i.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/itrc_sos_test_sw_regions.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/itrc_sos_test_sw_io.sql"
+# su postgres -c "psql -d vagrant -f /vagrant/models/solid_waste/db/data/itrc_sos_test_sw_o.sql"
+
+# Upgrade - with non-interactive flags so grub/sudo don't hang on config changes
+DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" \
+    -o Dpkg::Options::="--force-confold" -yq upgrade
+
+# Add package repository for dotnet core
+echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list
+apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+apt-get update
+# Install dotnet core (version latest as of 2016-11-28)
+apt-get install -y dotnet-dev-1.0.0-preview2.1-003177 --allow-unauthenticated
+
+
+# Run the tests for the solid waste model
+cd /vagrant/models/solid_waste/src/SolidWasteModel
+dotnet restore
+cd /vagrant/models/solid_waste/test/SolidWasteModel.Tests
+dotnet restore
+dotnet test
