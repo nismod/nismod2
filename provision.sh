c3fa2cb9e2549ebc751334ff468f8af85a266c9e
Tom Russell <tomalrussell@gmail.com>
2017-02-02 16:26:41 +0000

    provision: add postgres and solid waste

--- provision.sh
+++ provision.sh
@@ -1,7 +1,19 @@
 # Update package lists
 apt-get update
 # Install OS packages
-apt-get install -y build-essential git vim-nox python3-pip python3
+apt-get install -y build-essential git vim-nox python3-pip python3 postgresql \
+    postgresql-contrib
+
+# Database config to listen on network connection
+sed -i "s/#listen_address.*/listen_addresses 'localhost'/" \
+    /etc/postgresql/9.5/main/postgresql.conf
+# Create vagrant role if not exists
+su postgres -c "psql -c \"SELECT 1 FROM pg_user WHERE usename = 'vagrant';\" " \
+    | grep -q 1 || su postgres -c "psql -c \"CREATE ROLE vagrant SUPERUSER LOGIN PASSWORD 'vagrant';\" "
+# Create vagrant database if not exists
+su postgres -c "psql -c \"SELECT 1 FROM pg_database WHERE datname = 'vagrant';\" " \
+    | grep -q 1 || su postgres -c "createdb -E UTF8 -T template0 --locale=en_US.utf8 -O vagrant vagrant"
+
 # Upgrade - with non-interactive flags so grub/sudo don't hang on config changes
 DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" \
     -o Dpkg::Options::="--force-confold" -yq upgrade
@@ -17,38 +29,8 @@ chown vagrant:vagrant /home/vagrant/.bashrc
 pip install git+https://github.com/nismod/smif
 
 
-# Provision script for the transport model
+# Provision transport model
 bash /vagrant/provision/transport.sh
 
-# Provision script for the solid waste model
-
-# Create roles for solid waste database if not exists
-su postgres -c "psql -c \"SELECT 1 FROM pg_user WHERE usename = 'runcdamrole';\" " \
-    | grep -q 1 || su postgres -c "psql -c \"CREATE ROLE runcdamrole;\" "
-
-su postgres -c "psql -c \"SELECT 1 FROM pg_user WHERE usename = 'sosadministratorrole';\" " \
-    | grep -q 1 || su postgres -c "psql -c \"CREATE ROLE sosadministratorrole;\" "
-
-su postgres -c "psql -c \"SELECT 1 FROM pg_user WHERE usename = 'djrole';\" " \
-    | grep -q 1 || su postgres -c "psql -c \"CREATE ROLE djrole;\" "
-
-su postgres -c "psql -c \"SELECT 1 FROM pg_user WHERE usename = 'scr1';\" " \
-    | grep -q 1 || su postgres -c "psql -c \"CREATE ROLE scr1;\" "
-
-
-# Upgrade - with non-interactive flags so grub/sudo don't hang on config changes
-DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" \
-    -o Dpkg::Options::="--force-confold" -yq upgrade
-
-# Add package repository for dotnet core
-echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list
-apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
-apt-get update
-# Install dotnet core (version latest as of 2016-11-28)
-apt-get install -y dotnet-dev-1.0.0-preview2.1-003177 --allow-unauthenticated
-
-# Restore dependencies for the solid waste model
-cd /vagrant/models/solid_waste/src/SolidWasteModel
-dotnet restore
-cd /vagrant/models/solid_waste/test/SolidWasteModel.Tests
-dotnet restore
+# Provision solid_waste model
+bash /vagrant/provision/solid_waste.sh
