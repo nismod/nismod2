ead7ad4a07cfbc78882ee7babd963d90e9c05346
Craig Robson <craig.robson1@ncl.ac.uk>
2018-02-27 15:22:02 +0000

    copy from ftp server added for the project data directories

--- get_datafiles.py
+++ get_datafiles.py
@@ -18,5 +18,20 @@ with pysftp.Connection('ceg-itrc.ncl.ac.uk', username=username, password=passwor
 	# get data files
 	with sftp.cd('project/data'):
 
-		# get folder of data
-		sftp.get_d('region_definitions')
+		# get region_definitions
+		sftp.get_r('region_definitions', 'data/region_definitions')
+
+		# get initial_conditions
+		sftp.get_r('initial_conditions', 'data/initial_conditions')
+
+		# get interventions
+		sftp.get_r('interventions', 'data/interventions')
+
+		# get interval_definitions
+		sftp.get_r('interval_definitions', 'data/interval_definitions')
+
+		# get narratives
+		sftp.get_r('narratives', 'data/narratives')
+
+		# get scenarios
+		sftp.get_r('scenarios', 'data/scenarios')
