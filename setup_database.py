b525919e79da26d97ab51ec0c9ca970a3e9a14df
Craig Robson <craig.robson1@ncl.ac.uk>
2018-02-27 14:16:23 +0000

    database setup py file loads and installd api given correct user details

--- setup_database.py
+++ setup_database.py
@@ -1,1 +1,39 @@
 # sets up the nismod database
+# uses the SFTP server to get datafiles for database
+
+import pysftp, re, subprocess
+from getpass import getpass
+
+# ask for SFTP username
+username = input("Username:")
+# ask for SFTP password
+password = getpass("Enter password for " + username + ":")
+
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
+
+# establish connection to SFTP server
+with pysftp.Connection('ceg-itrc.ncl.ac.uk', username=username, password=password, cnopts=cnopts) as sftp:
+
+	# get api wheel
+	with sftp.cd('project/database'):
+
+		# get list of files
+		file_names = sftp.listdir()
+
+		# find wheel file
+		for file in file_names:
+			wheel = re.search(".whl", file)
+
+			# when found, get from server
+			if wheel:
+				sftp.get(file)
+
+				# install api from wheel file
+				subprocess.run(['sudo', 'pip3', 'install', file])
+
+				# end search
+				break
+
+	# get database files
+
