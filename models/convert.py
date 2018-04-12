1b9b4b422a12cac3ee00b4a47643a0b7e90449f6
Will Usher <william.usher@ouce.ox.ac.uk>
2018-04-12 17:09:03 +0100

    Added configuration for full energy demand-supply sos model

---
+++ models/convert.py
@@ -0,0 +1,74 @@
+
+"""Energy supply wrapper
+"""
+import numpy as np
+from smif.model.sector_model import SectorModel
+from subprocess import check_output
+import os
+import psycopg2
+from collections import namedtuple
+from smif.convert import SpaceTimeUnitConvertor
+import logging
+
+class InputOutputConvertor(SpaceTimeUnitConvertor):
+
+    def __init__(self):
+        super().__init__()
+
+    def convert_data(self, data_handle, model_input, model_output):
+
+        self.logger.info("Converting data for input '%s'",
+                         model_input.name)
+
+        data = data_handle.get_data(model_input.name)
+
+        from_spatial = model_input.spatial_resolution.name
+        from_temporal = model_input.temporal_resolution.name
+        from_unit = model_input.units
+
+        to_spatial = model_output.spatial_resolution.name
+        to_temporal = model_output.temporal_resolution.name
+        to_unit = model_output.units
+
+        self.logger.debug("Converting from %s to %s",
+            from_temporal, to_temporal)
+
+        self.logger.debug("Coverting from %s to %s",
+            from_spatial, to_spatial)
+
+        self.logger.debug("Coverting from %s to %s",
+            from_unit, to_unit)
+
+        space_time_result = SpaceTimeUnitConvertor.convert(
+            self,
+            data, 
+            from_spatial, 
+            to_spatial, 
+            from_temporal, 
+            to_temporal,
+            from_unit,
+            to_unit)
+        
+        return space_time_result
+
+class ConvertDemandToSupply(SectorModel):
+    """Converts energy demand data to energy supply
+    """
+    def initialise(self, initial_conditions):
+        pass
+
+    def simulate(self, data_handle):
+
+        convertor = InputOutputConvertor()
+
+        for input_name, model_input in self.inputs.items():
+            self.logger.debug("Model input: %s", model_input)
+            model_output = self.outputs[input_name]
+            results = convertor.convert_data(data_handle,
+                                             model_input,
+                                             model_output)
+            
+            data_handle.set_results(input_name, results)
+
+    def extract_obj(arg):
+        pass
