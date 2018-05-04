a598514294eb22a910651d312fab1c566cdce108
roaldl <roald.lemmen@ouce.ox.ac.uk>
2018-05-04 09:38:54 +0100

    Add digital_comms region sets to provision script

--- provision/digital_comms.sh
+++ provision/digital_comms.sh
@@ -9,4 +9,10 @@ source <(grep = <(grep -A3 '\[ftp-config\]' /vagrant/provision/ftp.ini))
 # Prepare directory for data
 mkdir -p "$target"
 
+# Download data
 . /vagrant/provision/get_data.sh digital-comms
+
+# Copy region definitions to smif region_definition
+mkdir /vagrant/data/region_definitions/assets_broadband_network
+cp /vagrant/data/digital_comms/assets_layer3_cabinets.* /vagrant/data/region_definitions/assets_broadband_network/
+cp /vagrant/data/digital_comms/assets_layer4_distributions.* /vagrant/data/region_definitions/assets_broadband_network/
