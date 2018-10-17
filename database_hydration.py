f41aff00ab80c6d7ac6d95a7e7eb0e28252d5259
Craig Robson <craig.robson1@ncl.ac.uk>
2018-10-17 14:37:35 +0100

    functionalised add intervention process further. now used by initial conditions hydration process also

--- database_hydration.py
+++ database_hydration.py
@@ -100,6 +100,76 @@ class HydrateDatabase:
 
 		return
 
+	def add_intervention(self, intervention, sector_id, sector_name):
+		"""
+
+		:param intervention:
+		:param sector_id:
+		:param sector_name:
+		:param relation_name:
+		:return:
+		"""
+		# relation_name
+		relation_name = 'interventions_' + sector_name
+
+		### add intervention to the interventions relation
+		# get intervention name - what happens if no such key?
+		intervention_name = intervention['name']
+
+		# get the intervention details
+		intervention_details = str(intervention)
+
+		# run insert statement to add intervention
+		self.db_cursor.execute('INSERT INTO interventions (sector, name, details) VALUES (%s,%s,%s)',
+							   [sector_id, intervention_name, intervention_details])
+
+		### add to the sector specific interventions relation
+		# check if a interventions relation for the sector exists
+		self.check_sector_intervention_relation_exists(sector_name)
+
+		# get column list from yml
+		# get value list
+		column_list = ''
+		value_list = ''
+
+		# loop through the keys for the intervention
+		for key in intervention.keys():
+
+			# check if an attribute, or if a dictionary with sub-attributes
+			if isinstance(intervention[key], dict):
+				# loop through the sub-attributes
+				for subkey in intervention[key].keys():
+
+					# form value list
+					value_list, add_column = add_to_value_string(intervention[key][subkey], value_list)
+
+					# form column list
+					if add_column is True:
+						column_list += "%s," % (str(key) + '_' + str(subkey))
+
+						# check for column name and add them if missing
+						self.check_column_exists(relation_name, (str(key) + '_' + str(subkey)))
+			else:
+				# form value list
+				value_list, add_column = add_to_value_string(intervention[key], value_list)
+
+				# form column list
+				if add_column is True:
+					column_list += "%s," % key
+
+					# check for column name and add them if missing
+					self.check_column_exists(relation_name, key)
+
+		# check if intervention type is in the types table - if not add
+		# this will need updating depending on what format the initial conditions will be uploaded in
+		subprocess.run(['psql', '-U', 'vagrant', '-d', 'nismod_smif', '-c',	'INSERT INTO intervention_type (name) SELECT (\'%s\') WHERE NOT EXISTS (SELECT id FROM intervention_type WHERE name=\'%s\');' % (intervention['name'], intervention['name'])])
+
+		# insert intervention into interventions table - get intervention type id and add to table
+		# subprocess.run(['psql', '-U', 'vagrant', '-d', 'nismod_smif', '-c','INSERT INTO %s (%s,sector,intervention_type_id) VALUES (%s,(SELECT id FROM sectors WHERE name=\'%s\'),(SELECT id FROM intervention_type WHERE name=\'%s\'));' % ('interventions_'+sector_name,column_list[:-1], value_list[:-1], sector_name, intervention['name'])])
+		subprocess.run(['psql', '-U', 'vagrant', '-d', 'nismod_smif', '-c',	'INSERT INTO %s (%s, sector) VALUES (%s,%s);' % ('interventions_' + sector_name, column_list[:-1], value_list[:-1], sector_id)])
+
+		return
+
 	def region_definitions(self, data_dir):
 		'''
 		imports regions into database
@@ -204,68 +274,12 @@ class HydrateDatabase:
 			# get the sector id
 			sector_id = self.get_sector_id(sector_name)
 
-			# relation_name
-			relation_name = 'interventions_'+sector_name
-
 			# loop through the interventions
 			for intervention in data:
 
-				### add intervention to the interventions relation
-				# get intervention name - what happens if no such key?
-				intervention_name = intervention['name']
+				# add an intervention to the database
+				self.add_intervention(intervention, sector_id, sector_name)
 
-				# get the intervention details
-				intervention_details = str(intervention)
-
-				# run insert statement to add intervention
-				self.db_cursor.execute('INSERT INTO interventions (sector, name, details) VALUES (%s,%s,%s)', [sector_id, intervention_name, intervention_details])
-
-				### add to the sector specific interventions relation
-				# check if a interventions relation for the sector exists
-				self.check_sector_intervention_relation_exists(sector_name)
-
-
-				# get column list from yml
-				# get value list
-				column_list = ''
-				value_list = ''
-
-				# loop through the keys for the intervention
-				for key in intervention.keys():
-
-					# check if an attribute, or if a dictionary with sub-attributes
-					if isinstance(intervention[key], dict):
-						# loop through the sub-attributes
-						for subkey in intervention[key].keys():
-
-							# form value list
-							value_list, add_column = add_to_value_string(intervention[key][subkey], value_list)
-
-							# form column list
-							if add_column is True:
-								column_list += "%s," % (str(key) + '_' + str(subkey))
-
-								# check for column name and add them if missing
-								self.check_column_exists(relation_name, (str(key) + '_' + str(subkey)))
-					else:
-						# form value list
-						value_list, add_column = add_to_value_string(intervention[key], value_list)
-
-						# form column list
-						if add_column is True:
-							column_list += "%s," % key
-
-							# check for column name and add them if missing
-							self.check_column_exists(relation_name, key)
-
-
-				# check if intervention type is in the types table - if not add
-				# this will need updating depending on what format the initial conditions will be uploaded in
-				subprocess.run(['psql', '-U', 'vagrant', '-d', 'nismod_smif', '-c', 'INSERT INTO intervention_type (name) SELECT (\'%s\') WHERE NOT EXISTS (SELECT id FROM intervention_type WHERE name=\'%s\');' % (intervention['name'], intervention['name'])])
-
-				# insert intervention into interventions table - get intervention type id and add to table
-				#subprocess.run(['psql', '-U', 'vagrant', '-d', 'nismod_smif', '-c','INSERT INTO %s (%s,sector,intervention_type_id) VALUES (%s,(SELECT id FROM sectors WHERE name=\'%s\'),(SELECT id FROM intervention_type WHERE name=\'%s\'));' % ('interventions_'+sector_name,column_list[:-1], value_list[:-1], sector_name, intervention['name'])])
-				subprocess.run(['psql', '-U', 'vagrant', '-d', 'nismod_smif', '-c', 'INSERT INTO %s (%s, sector) VALUES (%s,%s);' % ('interventions_'+sector_name, column_list[:-1], value_list[:-1], sector_id)])
 		return
 
 	def initial_conditions(self, data_dir):
@@ -299,60 +313,12 @@ class HydrateDatabase:
 			# get sector name
 			sector_name = item.split('.')[0]
 
+			# get sector id
+			sector_id = self.get_sector_id(sector_name)
+
 			# loop through the conditions
-			for condition in data:
-
-				# get column list from yml
-				# get value list
-				column_list = ''
-				value_list = ''
-				existing_int_col_list = ''
-				existing_int_val_list = ''
-
-				# loop through the keys for the conditions
-				for key in condition.keys():
-
-					# check if an attribute, or if a dictionary with sub-attributes
-					if isinstance(condition[key], dict):
-
-						# loop through the sub-attributes
-						for subkey in condition[key].keys():
-
-							# form column list
-							column_list += "%s," % (str(key) + '_' + str(subkey))
-
-							# form value list
-							if isinstance(condition[key][subkey], str):
-								value_list += "'%s'," % (condition[key][subkey])
-							else:
-								value_list += "%s," % (condition[key][subkey])
-
-					else:
-						# build date not part of interventions table so in different list for second part of hydration process
-						if key == 'build_date':
-							# form column list
-							existing_int_col_list += "%s," % key
-
-							# form value list
-							if isinstance(condition[key], str):
-								existing_int_val_list += "'%s'," % (condition[key])
-							else:
-								existing_int_val_list += "%s," % (condition[key])
-						else:
-							# form column list
-							column_list += "%s," % key
-
-							# form value list
-							if isinstance(condition[key], str):
-								value_list += "'%s'," % (condition[key])
-							else:
-								value_list += "%s," % (condition[key])
-
-				# insert intervention into interventions table
-				subprocess.run(['psql', '-U', 'vagrant', '-d', 'nismod_smif', '-c','INSERT INTO interventions (%s,sector) VALUES (%s,(SELECT id FROM sectors WHERE name=\'%s\'));' % (column_list[:-1], value_list[:-1], sector_name)])
-
-				# insert id into existing interventions table (get id from sequence)
-				subprocess.run(['psql', '-U', 'vagrant', '-d', 'nismod_smif', '-c','INSERT INTO interventions_existing (%s,intervention_id) VALUES (%s,(SELECT max(id) FROM interventions));' % (existing_int_col_list[:-1], existing_int_val_list[:-1])])
+			for intervention in data:
+				self.add_intervention(intervention, sector_id, sector_name)
 
 		return
 
