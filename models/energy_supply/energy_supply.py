2c42c2dfa18575586926babe24bccb8d56367687
Will Usher <william.usher@ouce.ox.ac.uk>
2019-01-29 14:16:38 +0000

    Fixed syntax

--- models/energy_supply/energy_supply.py
+++ models/energy_supply/energy_supply.py
@@ -209,7 +209,7 @@ class EnergySupplyWrapper(SectorModel):
             nismod_dir, 'energy_supply', 'model')))
         self.logger.debug("\n\n***Running the Energy Supply Model***\n\n")
         arguments = [self.get_model_executable()]
-        self.logger.debug(check_output(['mosel', 'exec', arguments))
+        self.logger.debug(check_output(['mosel', 'exec', arguments]))
 
     def retrieve_outputs(self, data, now):
         """Retrieves results from the model
