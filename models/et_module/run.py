bc5c2bf424a2d67265023e571a4ddbc2c317df87
Will Usher <william.usher@ouce.ox.ac.uk>
2019-02-26 16:03:58 +0000

    Added test model run for et_module

--- models/et_module/run.py
+++ models/et_module/run.py
@@ -1,41 +1,13 @@
 """The sector model wrapper for smif to run the energy demand model
 """
-import os
-import logging
-import configparser
-import numpy as np
-
+from smif.model import SectorModel
 from et_module.main import main
 
-REGION_SET_NAME = 'lad_uk_2016'
+REGION_SET_NAME = 'lad_gb_2016'
 
 class ETWrapper(SectorModel):
-    """Energy Demand Wrapper
+    """Energy-Transport Model Wrapper
     """
-    def __init__(self, name):
-        super().__init__(name)
-        self.user_data = {}
-
-    def array_to_dict(self, input_array):
-        """Convert array to dict
-
-        Arguments
-        ---------
-        input_array : numpy.ndarray
-            timesteps, regions, interval
-
-        Returns
-        -------
-        output_dict : dict
-            timesteps, region, interval
-
-        """
-        output_dict = defaultdict(dict)
-        for r_idx, region in enumerate(self.get_region_names(REGION_SET_NAME)):
-            output_dict[region] = input_array[r_idx, 0]
-
-        return dict(output_dict)
-
     def before_model_run(self, data_handle=None):
         """Implement this method to conduct pre-model run tasks
 
@@ -48,11 +20,6 @@ class ETWrapper(SectorModel):
         Info
         -----
         `self.user_data` allows to pass data from before_model_run to main model
-        """
-        pass
-
-    def initialise(self, initial_conditions):
-        """
         """
         pass
 
@@ -70,22 +37,20 @@ class ETWrapper(SectorModel):
         et_module_out : dict
             Outputs of et_module
         """
-        regions = data_handle.get_region_names(REGION_SET_NAME)
-
         simulation_yr = data_handle.current_timestep
 
         # Read number of EV trips starting in regions (np.array(regions, 24h))
-        reg_trips_ev_24h = data_handle.get_data('trips')
+        reg_trips_ev_24h = data_handle.get_data('ev_trips')
+        
+        # Obtain the list of regions from the model input
+        regions = reg_trips_ev_24h.dim_coords(REGION_SET_NAME).ids
 
         # Get hourly demand data for day for every region (np.array(regions, 24h)) (kWh)
-        reg_elec_24h = data_handle.get_data('electricity')
+        reg_elec_24h = data_handle.get_data('ev_electricity')
 
         actual_v2g_capacity = main(regions, 
                                    simulation_yr, 
-                                   reg_trips_ev_24h, 
-                                   reg_elec_24h)
+                                   reg_trips_ev_24h.as_ndarray(), 
+                                   reg_elec_24h.as_ndarray())
 
-        data_handle.set_results('capacity', actual_v2g_capacity)
-
-    def extract_obj(self, results):
-        return 0
+        data_handle.set_results('v2g_g2v_capacity', actual_v2g_capacity)
