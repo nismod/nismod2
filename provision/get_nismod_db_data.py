32d5538076439d5c29edaad1c30ba2d3f0f8a063
Tom Russell <tomalrussell@gmail.com>
2019-03-07 16:14:38 +0000

    Write to smif DataStores when fetching nismod db data

--- provision/get_nismod_db_data.py
+++ provision/get_nismod_db_data.py
@@ -18,9 +18,13 @@ import shutil
 import pandas
 import requests
 
+from smif.data_layer.file import CSVDataStore, ParquetDataStore
+from smif.data_layer.data_array import DataArray
+from smif.metadata import Spec
+
 
 CACHE_PATH = os.path.join(os.path.dirname(__file__), 'cache')
-SCENARIOS_PATH = os.path.join(os.path.dirname(__file__), '..', 'data', 'scenarios')
+STORE_BASE_PATH = os.path.join(os.path.dirname(__file__), '..')
 PERSISTENT_DATA_PATH = os.path.join(os.path.dirname(__file__), 'persistent_data')
 POPULATION_MIN_YEAR = 2011
 POPULATION_MAX_YEAR = 2020
@@ -50,23 +54,48 @@ def main():
     process_oa_population()
     process_oa_to_lad_population()
 
-    # Move to data/scenarios folder
-    shutil.move(
-        os.path.join(CACHE_PATH, 'oa_population.csv'),
-        os.path.join(SCENARIOS_PATH, 'population_nismod_db.v5_oa.csv'),
-    )
-    shutil.move(
-        os.path.join(CACHE_PATH, 'lad_population.csv'),
-        os.path.join(SCENARIOS_PATH, 'population_nismod_db.v5_lad16.csv'),
+    # Read
+    oa_pop = pandas.read_csv(os.path.join(CACHE_PATH, 'oa_population.csv'))
+    lad_pop = pandas.read_csv(os.path.join(CACHE_PATH, 'lad_population.csv'))
+
+    # to DataArray
+    years = list(range(POPULATION_MIN_YEAR, POPULATION_MAX_YEAR + 1))
+    oas = list(oa_pop.oa.unique())
+    oa_spec = Spec(
+        name='population',
+        dims=['timestep', 'oa'],
+        coords={'oa': oas, 'timestep': years},
+        dtype='int'
     )
 
+    lads = list(lad_pop.lad16cd.unique())
+    lad_spec = Spec(
+        name='population',
+        dims=['timestep', 'lad16cd'],
+        coords={'lad16cd': lads, 'timestep': years},
+        dtype='int'
+    )
+
+    # Write to CSV and Parquet stores
+    text_store = CSVDataStore(STORE_BASE_PATH)
+    binary_store = ParquetDataStore(STORE_BASE_PATH)
+
+    lad_key = 'population_nismod_db.v5_lad16'
+    lad_pop_da = DataArray.from_df(lad_spec, lad_pop.set_index(lad_spec.dims))
+    text_store.write_scenario_variant_data('{}.csv'.format(lad_key), lad_pop_da)
+    binary_store.write_scenario_variant_data('{}.parquet'.format(lad_key), lad_pop_da)
+
+    oa_key = 'population_nismod_db.v5_oa'
+    oa_pop_da = DataArray.from_df(oa_spec, oa_pop.set_index(oa_spec.dims))
+    text_store.write_scenario_variant_data('{}.csv'.format(oa_key), oa_pop_da)
+    binary_store.write_scenario_variant_data('{}.parquet'.format(oa_key), oa_pop_da)
 
 def get_population(auth):
     pop_url = 'https://www.nismod.ac.uk/api/data/households/population_totals'
     for year in range(POPULATION_MIN_YEAR, POPULATION_MAX_YEAR + 1):
-        print("Get oa population", year)
         oa_pop_file = get_oa_population_file(year)
         if not os.path.exists(oa_pop_file):
+            print("Get oa population", year)
             r = requests.get(
                 pop_url,
                 auth=auth,
@@ -92,16 +121,17 @@ def process_oa_population():
     with open(oa_csv_path, 'w', newline='') as oa_fh:
         oa_writer = csv.DictWriter(
             oa_fh,
-            fieldnames=('oa', 'population', 'year')
+            fieldnames=('oa', 'population', 'timestep')
         )
         oa_writer.writeheader()
         for year in range(POPULATION_MIN_YEAR, POPULATION_MAX_YEAR + 1):
+            print("Process oa population", year)
             with open(get_oa_population_file(year), 'r') as year_fh:
                 data = json.load(year_fh)
                 oa_writer.writerows({
                     'oa': d['oa'],
-                    'population': d['total_population'],
-                    'year': d['year']
+                    'population': int(d['total_population']),
+                    'timestep': int(d['year'])
                 } for d in data)
 
 
@@ -118,16 +148,16 @@ def process_oa_to_lad_population():
     lad11_pop = oa_pop.merge(
         oa_to_lad, left_on='oa', right_on='OA'
     )[
-        ['oa', 'population', 'year', 'LAD']
+        ['oa', 'population', 'timestep', 'LAD']
     ].groupby(
-        ['year', 'LAD']
+        ['timestep', 'LAD']
     ).sum().reset_index()
 
     # merge LAD11 population with changed codes lookup, output with LAD16 codes
     lad16_pop = lad11_pop.merge(
         lad_to_lad[['lad11cd', 'lad16cd']], left_on='LAD', right_on='lad11cd'
     )[
-        ['year', 'lad16cd', 'population']
+        ['timestep', 'lad16cd', 'population']
     ]
     lad16_pop.to_csv(os.path.join(CACHE_PATH, 'lad_population.csv'), index=False)
 
