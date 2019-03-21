92fe2d6ad4842558eaf4d96a89c0ca248e107a38
Tom Russell <tomalrussell@gmail.com>
2019-03-21 11:31:52 +0000

    Add ES migration-runner script

---
+++ provision/run_energy_supply_migrations.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#
+# Run/re-run energy supply database migrations
+#
+
+# Usage, e.g. from nismod repository root:
+#   bash ./provision/run_energty_supply_migrations.sh . full
+base_path=$1
+minimal_or_full=$2
+
+# Read dbname, host, user, password, port from dbconfig.ini
+source <(grep = <(grep -A5 "\[energy-supply\]" $base_path/provision/dbconfig.ini))
+
+# Read model_version, remote_data, local_dir from config.ini
+source <(grep = <(grep -A3 "\[energy-supply\]" $base_path/provision/config.ini))
+
+MODEL_DIR=$base_path/install
+DATA_DIR=$base_path/$local_dir
+MIGRATIONS_DIR=$MODEL_DIR/energy_supply/migrations
+
+# Run migrations
+PGPASSWORD=$password \
+PGHOST=$host \
+PGUSER=$user \
+PGPORT=$port \
+PGDATABASE=$dbname \
+    python $MODEL_DIR/energy_supply/run_migrations.py \
+    -r $DATA_DIR/database_$minimal_or_full $MIGRATIONS_DIR
