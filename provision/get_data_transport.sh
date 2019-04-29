e6a4908ac5e4488183aff0646458c1e6ba7ee4d2
Tom Russell <tomalrussell@gmail.com>
2019-04-29 13:41:54 +0100

    Move transport data to consistent location

--- provision/get_data_transport.sh
+++ provision/get_data_transport.sh
@@ -4,11 +4,19 @@
 base_path=$1
 
 # Read model_version, remote_data, local_dir from config.ini
-source <(grep = <(grep -A3 "\[transport\]" $base_path/provision/config.ini))
+source <(grep = <(grep -A4 "\[transport\]" $base_path/provision/config.ini))
 
 # Download data
 python $base_path/provision/get_data.py $remote_data $base_path/$local_dir
+mv $base_path/$local_dir/TR_data_full_* $base_path/$local_dir/TR_data_full
+rm -r $base_path/$local_dir/gb
+mkdir -p $base_path/$local_dir/gb/config
+mv $base_path/$local_dir/TR_data_full/full/data $base_path/$local_dir/gb/data
 
 # Download test (Southampton) data
 source <(grep = <(grep -A3 "\[transport-test\]" $base_path/provision/config.ini))
 python $base_path/provision/get_data.py $remote_data $base_path/$local_dir
+mv $base_path/$local_dir/transport_testdata* $base_path/$local_dir/transport_testdata
+rm -r $base_path/$local_dir/southampton
+mkdir -p $base_path/data/transport/southampton/config
+mv $base_path/$local_dir/transport_testdata $base_path/$local_dir/southampton/data
