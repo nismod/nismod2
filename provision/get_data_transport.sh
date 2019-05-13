d768bc26ec626315beab8210df38604b41d3c1bf
Tom Russell <tomalrussell@gmail.com>
2019-05-13 13:41:20 +0100

    Add engine type fractions to transport provisioning

--- provision/get_data_transport.sh
+++ provision/get_data_transport.sh
@@ -6,6 +6,7 @@ base_path=$1
 # Download full (Great Britain) data
 # Read model_version, remote_data, local_dir from config.ini
 source <(grep = <(grep -A3 "\[transport\]" $base_path/provision/config.ini))
+rm -rf $base_path/$local_dir/TR_data_full
 python $base_path/provision/get_data.py $remote_data $base_path/$local_dir
 mv $base_path/$local_dir/TR_data_full_for_release_$model_version $base_path/$local_dir/TR_data_full
 rm -rf $base_path/$local_dir/gb
@@ -24,3 +25,21 @@ mv $base_path/$local_dir/transport_testdata $base_path/$local_dir/southampton/da
 # gzip routes as expected by model
 gzip $base_path/$local_dir/southampton/data/routes/passengerRoutes.dat
 gzip $base_path/$local_dir/southampton/data/routes/freightRoutes.dat
+
+
+# Run conversion to smif scenarios
+
+# Baseline
+python $base_path/utilities/transport/convert_transport_engine_fractions.py \
+    $base_path/$local_dir/gb/data/csvfiles/engineTypeFractions.csv \
+    $base_path/data/scenarios/engine_type_fractions.csv
+
+# EW - electric world
+python $base_path/utilities/transport/convert_transport_engine_fractions.py \
+    $base_path/$local_dir/gb/data/csvfiles/engineTypeFractionsEW.csv \
+    $base_path/data/scenarios/engine_type_fractions_ew.csv
+
+# MVE - multi-vector
+python $base_path/utilities/transport/convert_transport_engine_fractions.py \
+    $base_path/$local_dir/gb/data/csvfiles/engineTypeFractionsMVE.csv \
+    $base_path/data/scenarios/engine_type_fractions_mve.csv
