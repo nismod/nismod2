11411d6fa988b9b7b4af177b400ffcb00f1ccca4
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2019-07-15 19:37:57 +0100

    Move NLC to first column in interventions file

--- utilities/write_rail_interventions.sh
+++ utilities/write_rail_interventions.sh
@@ -5,12 +5,13 @@ outputInterventionFile=$2
 if [ -f $inputFile ]; then
     rm -i $outputInterventionFile
 fi
-first_columns=name,type,technical_lifetime_value,techinal_lifetime_units,
-echo $first_columns$(head -1 $inputFile | cut -d, -f1-6,9-12) > $outputInterventionFile
+first_columns=NLC_southampton,name,type,technical_lifetime_value,techinal_lifetime_units,
+echo $first_columns$(head -1 $inputFile | cut -d, -f2-6,9-12) > $outputInterventionFile
 
 nbLines=$(wc -l $inputFile | cut -d' ' -f1)
 for line in $(tail -$(($nbLines-1)) $inputFile)
 do
     name=new$(echo $line | cut -d, -f3)
-    echo $name,NewRailStation,100,y,$(echo $line | cut -d, -f1-6,9-12) >> $outputInterventionFile
+    NLCvalue=$(echo $line | cut -d, -f1)
+    echo $NLCvalue,$name,NewRailStation,100,y,$(echo $line | cut -d, -f2-6,9-12) >> $outputInterventionFile
 done
