f73276e561994e2022e36c997b096a83120f6dfa
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2019-07-22 09:18:56 +0100

    Add comments for interventions to init. cond. script

--- utilities/transport/rail_in_cond_from_int.sh
+++ utilities/transport/rail_in_cond_from_int.sh
@@ -1,3 +1,19 @@
+# Generate initial conditions file from list of interventions.
+# Usage : bash rail_in_cond_from_int.sh
+# Input: interventions file
+
+# NLC_gb,name,type,..,LADcode,LADname,Area
+# 375,newEnerglyn_&_Churchill_Park_NRAIL,NewRailStation,...,W06000018,Caerphilly,OTHER
+# 500,newActon_Town_TUBE,NewRailStation,...,E09000009,Ealing,LT
+# ...
+
+# to
+
+# name, build_year
+# newEnerglyn_&_Churchill_Park_NRAIL, 1975
+# newActon_Town_TUBE, 1975
+# ...
+
 path_interventions=../../data/interventions/transport_rail.csv
 path_initial_conditions=../../data/initial_conditions/base_year_railstations.csv
 if [ -f "$path_initial_conditions" ] ; then
