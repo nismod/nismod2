4bf37b0692c1529902c4affcc161846945e4d3a2
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2019-07-22 08:34:09 +0100

    Disambiguate intervention names with mode for full gb run

--- utilities/write_rail_interventions.sh
+++ utilities/write_rail_interventions.sh
@@ -5,13 +5,16 @@ outputInterventionFile=$2
 if [ -f $inputFile ]; then
     rm -i $outputInterventionFile
 fi
-first_columns=NLC_southampton,name,type,technical_lifetime_value,technical_lifetime_units,
+first_columns=NLC_gb,name,type,technical_lifetime_value,technical_lifetime_units,
 echo $first_columns$(head -1 $inputFile | cut -d, -f2-6,9-12) > $outputInterventionFile
 
 nbLines=$(wc -l $inputFile | cut -d' ' -f1)
 for line in $(tail -$(($nbLines-1)) $inputFile)
 do
+    # Several interventions can have the same name, but different MODE (TUBE, NRAIL..)
+    # disambiguating by adding the mode name to the intervention name
+    mode=$(echo $line | cut -d, -f2)
     name=new$(echo $line | cut -d, -f3)
     NLCvalue=$(echo $line | cut -d, -f1)
-    echo $NLCvalue,$name,NewRailStation,100,y,$(echo $line | cut -d, -f2-6,9-12) >> $outputInterventionFile
+    echo $NLCvalue,${name}_$mode,NewRailStation,100,y,$(echo $line | cut -d, -f2-6,9-12) >> $outputInterventionFile
 done
