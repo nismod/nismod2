b1315aea6602ee475609e1fbc25b8fe8cf3d6f61
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2019-07-22 10:02:00 +0100

    base generation of initial conditions file on base year rail demand file instead of interventions file. This way future stations are not included

--- utilities/transport/rail_in_cond_from_int.sh
+++ utilities/transport/rail_in_cond_from_int.sh
@@ -1,10 +1,10 @@
-# Generate initial conditions file from list of interventions.
+# Generate initial conditions file from base year rail usage file
 # Usage : bash rail_in_cond_from_int.sh
-# Input: interventions file
+# Input: base year rail usage file
 
-# NLC_gb,name,type,..,LADcode,LADname,Area
-# 375,newEnerglyn_&_Churchill_Park_NRAIL,NewRailStation,...,W06000018,Caerphilly,OTHER
-# 500,newActon_Town_TUBE,NewRailStation,...,E09000009,Ealing,LT
+# NLC,Mode,Station,...,LADcode,LADname,Area
+# 375,NRAIL,Energlyn_&_Churchill_Park,..,W06000018,Caerphilly,OTHER
+# 500,TUBE,Acton_Town,Acton_Town_Underground_Station,...,E09000009,Ealing,LT
 # ...
 
 # to
@@ -14,17 +14,23 @@
 # newActon_Town_TUBE, 1975
 # ...
 
-path_interventions=../../data/interventions/transport_rail.csv
+path_baseYearRailUsage=../../data/transport/gb/data/csvfiles/baseYearRailUsage.csv
 path_initial_conditions=../../data/initial_conditions/base_year_railstations.csv
 if [ -f "$path_initial_conditions" ] ; then
     rm -i $path_initial_conditions
 fi
-
+if ! [ -f "$path_baseYearRailUsage" ] ; then
+    echo ERROR: Could not find base year rail usage file $path_baseYearRailUsage
+    exit 1
+fi
+echo Reading $path_baseYearRailUsage
 build_year=1975
-for line in $(cat $path_interventions)
+for line in $(cat $path_baseYearRailUsage)
 do
-    echo $(echo $line | cut -d, -f2),$build_year >> $path_initial_conditions
+    mode=$(echo $line | cut -d, -f2)
+    intervention_name=new$(echo $line | cut -d, -f3)_$mode
+    echo $intervention_name,$build_year >> $path_initial_conditions
 done
 
 # Update first line
-sed -i "1s/1975/build_year/" $path_initial_conditions
+sed -i "1s/.*/name,build_year/" $path_initial_conditions
