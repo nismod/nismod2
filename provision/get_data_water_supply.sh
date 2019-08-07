ccd5c4d0ef3a64584ca1e67807e27ff31cc1c623
Fergus Cooper <fcooper8472@gmail.com>
2019-08-07 19:03:17 +0100

    Tidy up bash scripts

--- provision/get_data_water_supply.sh
+++ provision/get_data_water_supply.sh
@@ -4,45 +4,45 @@
 base_path=$1
 
 # Define required directories and ensure they exist
-model_dir=${base_path}/models/water_supply
-download_dir=${model_dir}/download
-nodal_dir=${model_dir}/nodal
-dim_dir=${base_path}/data/dimensions/water_supply
-scenario_dir=${base_path}/data/scenarios/water_supply
-parameter_dir=${base_path}/data/parameters/water_supply
+model_dir="${base_path}"/models/water_supply
+download_dir="${model_dir}"/download
+nodal_dir="${model_dir}"/nodal
+dim_dir="${base_path}"/data/dimensions/water_supply
+scenario_dir="${base_path}"/data/scenarios/water_supply
+parameter_dir="${base_path}"/data/parameters/water_supply
 
-mkdir -p ${download_dir}
-mkdir -p ${nodal_dir}
-mkdir -p ${dim_dir}
-mkdir -p ${scenario_dir}
-mkdir -p ${parameter_dir}
+mkdir -p "${download_dir}"
+mkdir -p "${nodal_dir}"
+mkdir -p "${dim_dir}"
+mkdir -p "${scenario_dir}"
+mkdir -p "${parameter_dir}"
 
 # Download data
 remote_data=water_supply/water_supply_data_v8.zip
-python ${base_path}/provision/get_data.py ${remote_data} ${download_dir}
+python "${base_path}"/provision/get_data.py ${remote_data} "${download_dir}"
 
 # Move all nodal input files
-mv ${download_dir}/installed_data/CatchmentIndex.csv ${nodal_dir}
-mv ${download_dir}/installed_data/master_dynatop_points.csv ${nodal_dir}
-mv ${download_dir}/installed_data/missing_data.csv ${nodal_dir}
-mv ${download_dir}/installed_data/WRZ_DI_DO.csv ${nodal_dir}
+mv "${download_dir}"/installed_data/CatchmentIndex.csv "${nodal_dir}"
+mv "${download_dir}"/installed_data/master_dynatop_points.csv "${nodal_dir}"
+mv "${download_dir}"/installed_data/missing_data.csv "${nodal_dir}"
+mv "${download_dir}"/installed_data/WRZ_DI_DO.csv "${nodal_dir}"
 
 # Move all dimension definitions
-mv ${download_dir}/dimensions/arc_names.csv ${dim_dir}
-mv ${download_dir}/dimensions/cams_names.csv ${dim_dir}
-mv ${download_dir}/dimensions/days_into_year.csv ${dim_dir}
-mv ${download_dir}/dimensions/demand_nodes.csv ${dim_dir}
-mv ${download_dir}/dimensions/demand_profile_zones.csv ${dim_dir}
-mv ${download_dir}/dimensions/global_variable_names.csv ${dim_dir}
-mv ${download_dir}/dimensions/nonpublic_use_codes.csv ${dim_dir}
-mv ${download_dir}/dimensions/reservoir_names.csv ${dim_dir}
+mv "${download_dir}"/dimensions/arc_names.csv "${dim_dir}"
+mv "${download_dir}"/dimensions/cams_names.csv "${dim_dir}"
+mv "${download_dir}"/dimensions/days_into_year.csv "${dim_dir}"
+mv "${download_dir}"/dimensions/demand_nodes.csv "${dim_dir}"
+mv "${download_dir}"/dimensions/demand_profile_zones.csv "${dim_dir}"
+mv "${download_dir}"/dimensions/global_variable_names.csv "${dim_dir}"
+mv "${download_dir}"/dimensions/nonpublic_use_codes.csv "${dim_dir}"
+mv "${download_dir}"/dimensions/reservoir_names.csv "${dim_dir}"
 
 # Move the scenario data
-mv ${download_dir}/scenarios/001_daily.csv ${nodal_dir} # will eventually be scenario
-mv ${download_dir}/scenarios/borehole_forcing_1974_to_2015.csv ${nodal_dir} # will eventually be scenario
-mv ${download_dir}/scenarios/National_WRSM_NatModel_logNSE_obs_11018_1.txt ${nodal_dir} # will eventually be scenario
-mv ${download_dir}/scenarios/reservoir_levels.csv ${scenario_dir}
+mv "${download_dir}"/scenarios/001_daily.csv "${nodal_dir}" # will eventually be scenario
+mv "${download_dir}"/scenarios/borehole_forcing_1974_to_2015.csv "${nodal_dir}" # will eventually be scenario
+mv "${download_dir}"/scenarios/National_WRSM_NatModel_logNSE_obs_11018_1.txt "${nodal_dir}" # will eventually be scenario
+mv "${download_dir}"/scenarios/reservoir_levels.csv "${scenario_dir}"
 
 # Move the parameters data
-mv ${download_dir}/parameters/demand_profiles.csv ${parameter_dir}
-mv ${download_dir}/parameters/nonpublic_water_demands.csv ${parameter_dir}
+mv "${download_dir}"/parameters/demand_profiles.csv "${parameter_dir}"
+mv "${download_dir}"/parameters/nonpublic_water_demands.csv "${parameter_dir}"
--- provision/install_water_supply.sh
+++ provision/install_water_supply.sh
@@ -4,31 +4,31 @@
 base_path=$1
 
 # Define required directories and ensure they exist
-model_dir=${base_path}/models/water_supply
-repo_dir=${model_dir}/repo
-nodal_dir=${model_dir}/nodal
-exe_dir=${model_dir}/exe
+model_dir="${base_path}"/models/water_supply
+repo_dir="${model_dir}"/repo
+nodal_dir="${model_dir}"/nodal
+exe_dir="${model_dir}"/exe
 
-mkdir -p ${repo_dir}
-mkdir -p ${nodal_dir}
-mkdir -p ${exe_dir}
+mkdir -p "${repo_dir}"
+mkdir -p "${nodal_dir}"
+mkdir -p "${exe_dir}"
 
 # Clone repo and copy necessary files to the model directory
-git clone git@github.com:nismod/water_supply.git ${repo_dir} || exit "$?"
-pushd ${repo_dir}
+git clone git@github.com:nismod/water_supply.git "${repo_dir}" || exit
+pushd "${repo_dir}" || exit
     # Pin the model at a specific commit
     git checkout 02eca9f81e0bbd2ffdb7f4963e70da8163c24646
-popd
+popd || exit
 
 # Move the files necessary for execution
-mv ${repo_dir}/wathnet/w5_console.exe ${exe_dir}
-mv ${repo_dir}/models/National_Model.wat ${exe_dir}
+mv "${repo_dir}"/wathnet/w5_console.exe "${exe_dir}"
+mv "${repo_dir}"/models/National_Model.wat "${exe_dir}"
 
 # Seems to be necessary to add execution to the wathnet exe
-chmod +x ${exe_dir}/w5_console.exe
+chmod +x "${exe_dir}"/w5_console.exe
 
 # Move the prepare_nodal script
-mv ${repo_dir}/scripts/preprocessing/prepare_nodal.py ${nodal_dir}
+mv "${repo_dir}"/scripts/preprocessing/prepare_nodal.py "${nodal_dir}"
 
 # Clean up the cloned repo
-rm -rf ${repo_dir}
+rm -rf "${repo_dir}"
