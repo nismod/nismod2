d042dea428851a8b4ff0d4d56b6dfd848d048523
Fergus Cooper <fcooper8472@gmail.com>
2019-08-22 10:10:34 +0100

    Update to data v9

--- provision/get_data_water_supply.sh
+++ provision/get_data_water_supply.sh
@@ -18,7 +18,7 @@ mkdir -p "${scenario_dir}"
 mkdir -p "${parameter_dir}"
 
 # Download data
-remote_data=water_supply/water_supply_data_v8.zip
+remote_data=water_supply/water_supply_data_v9.zip
 python "${base_path}"/provision/get_data.py ${remote_data} "${download_dir}"
 
 # Move all nodal input files
@@ -29,18 +29,23 @@ mv "${download_dir}"/installed_data/WRZ_DI_DO.csv "${nodal_dir}"
 
 # Move all dimension definitions
 mv "${download_dir}"/dimensions/arc_names.csv "${dim_dir}"
+mv "${download_dir}"/dimensions/borehole_names.csv "${dim_dir}"
 mv "${download_dir}"/dimensions/cams_names.csv "${dim_dir}"
 mv "${download_dir}"/dimensions/days_into_year.csv "${dim_dir}"
 mv "${download_dir}"/dimensions/demand_nodes.csv "${dim_dir}"
 mv "${download_dir}"/dimensions/demand_profile_zones.csv "${dim_dir}"
+mv "${download_dir}"/dimensions/flow_file_column_names.csv "${dim_dir}"
 mv "${download_dir}"/dimensions/global_variable_names.csv "${dim_dir}"
+mv "${download_dir}"/dimensions/irrigations_cams_names.csv "${dim_dir}"
+mv "${download_dir}"/dimensions/irrigations_file_column_names.csv "${dim_dir}"
+mv "${download_dir}"/dimensions/months_into_year.csv "${dim_dir}"
 mv "${download_dir}"/dimensions/nonpublic_use_codes.csv "${dim_dir}"
 mv "${download_dir}"/dimensions/reservoir_names.csv "${dim_dir}"
 
 # Move the scenario data
-mv "${download_dir}"/scenarios/001_daily.csv "${nodal_dir}" # will eventually be scenario
-mv "${download_dir}"/scenarios/borehole_forcing_1974_to_2015.csv "${nodal_dir}" # will eventually be scenario
-mv "${download_dir}"/scenarios/National_WRSM_NatModel_logNSE_obs_11018_1.txt "${nodal_dir}" # will eventually be scenario
+mv "${download_dir}"/scenarios/borehole_data.csv "${scenario_dir}"
+mv "${download_dir}"/scenarios/flows_data.csv "${scenario_dir}"
+mv "${download_dir}"/scenarios/irrigations_data.csv "${scenario_dir}"
 mv "${download_dir}"/scenarios/reservoir_levels.csv "${scenario_dir}"
 
 # Move the parameters data
