aee59760bd6d4ea89a9808254e19fe3b2fc5cb62
Tom Russell <tomalrussell@gmail.com>
2019-10-01 17:26:45 +0100

    Try fixing get scenario data

--- provision/get_data_scenarios.sh
+++ provision/get_data_scenarios.sh
@@ -21,7 +21,10 @@ python $base_path/provision/get_data.py $remote_data $local_path
 # Dwellings, GVA, Population, Floor Area scenarios
 eval "$(grep -A3 "\[socio-economic\]" $base_path/provision/config.ini | tail -n3)"
 local_path=$base_path/$local_dir
+rm -r $local_path/socio-economic-*
 python $base_path/provision/get_data.py $remote_data $local_path
+mv $local_path/socio-economic-*/* $local_path
+rm -r $local_path/socio-economic-*
 
 # EV transport trips
 eval "$(grep -A3 "\[ev_transport_trips\]" $base_path/provision/config.ini | tail -n3)"
