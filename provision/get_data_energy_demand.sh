7a1cdf6c0c62f93a2db7e6282a79df9c2cb2f1bd
Tom Russell <tomalrussell@gmail.com>
2020-02-07 15:28:08 +0000

    Move copying data commands to get_data scripts

--- provision/get_data_energy_demand.sh
+++ provision/get_data_energy_demand.sh
@@ -12,3 +12,6 @@ python $base_path/provision/get_data.py $remote_data $local_path
 eval "$(grep -A3 "\[energy-demand-config-data\]" $base_path/provision/config.ini | tail -n3)"
 local_path=$base_path/$local_dir
 python $base_path/provision/get_data.py $remote_data $local_path
+
+# Copy lad_2016_uk_simplified.shp to a more general location as it is used by more than just energy_demand
+cp $base_path/data/energy_demand/energy_demand_minimal/region_definitions/lad_2016_uk_simplified.* $base_path/data/dimensions/
