b73709828a4a15fd9258784419855ea14c4c6458
Tom Russell <tomalrussell@gmail.com>
2020-04-24 17:02:03 +0100

    Add get_all.sh to get all data

---
+++ provision/get_all.sh
@@ -0,0 +1,6 @@
+bash -ex ./provision/get_data_digital_comms.sh .
+bash -ex ./provision/get_data_energy_demand.sh .
+bash -ex ./provision/get_data_energy_supply.sh .
+bash -ex ./provision/get_data_transport.sh .
+bash -ex ./provision/get_data_water_supply.sh .
+bash -ex ./provision/get_data_water_demand.sh .
